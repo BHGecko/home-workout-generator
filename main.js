ecea5bbfe44669f671f19498211e182beb09e3d6
adt-1111 <tabakadnan1112@gmail.com>
2025-12-05 22:31:43 +0100

    Add workout generator logic Implement random exercise picker

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 console.log("Home Workout Generator ready!");
+
+const exercises =  [
+  "Push-ups",
+  "Squats",
+  "Lunges",
+  "Jumping Jacks",
+  "Plank",
+  "Burpees",
+  "Mountain Climbers",
+  "High Knees",
+  "Glute Bridges",
+  "Tricep Dips",
+  "Bicycle Crunches",
+  "Russian Twists",
+  "Calf Raises",
+  "Wall Sit",
+  "Superman Hold"
+]
+
+function getRandomExercise (count = 5) {
+    const shuffled = [...exercises].sort(() => Math.random() - 0.5);
+    return shuffled.slice(0, count);
+}
+
+function displayWorkout() {
+  const workoutList = document.getElementById("workout-list");
+  workoutList.innerHTML = "";
+
+  const selected = getRandomExercises();
+
+  selected.forEach(ex => {
+    const li = document.createElement("li");
+    li.textContent = ex;
+    workoutList.appendChild(li);
+  });
+}
+
+document.getElementById("generate-btn").addEventListener("click", () => {
+  displayWorkout();
+});
